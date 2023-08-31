40dc6a181c06425d260bc8b12b30fd248abd296f
Umer Farooq <81526328+Umerhaqani90@users.noreply.github.com>
2023-08-31 16:14:48 +0500

    Initial commit

---
+++ AppService/Roles/RoleAppService.cs
@@ -0,0 +1,43 @@
+﻿using AppDomain;
+using AppDomain.Users;
+using AppService.Common;
+using AppService.Roles.Dto;
+using AppService.Users.Dto;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppService.Roles
+{
+    public interface IRoleAppService
+    {
+        Task<List<RoleDto>> GetAllRoles();
+    }
+    public class RoleAppService : IRoleAppService
+    {
+        private readonly IRepository<Role, long> _roleRepository;
+        private readonly IRepository<User, long> _userRepository;
+        #region Parameter
+        public RoleAppService(IRepository<Role, long> roleRepository, IRepository<User, long> userRepository)
+        {
+            this._roleRepository = roleRepository;
+            this._userRepository = userRepository;
+        }
+        #endregion
+        public async Task<List<RoleDto>> GetAllRoles()
+        {
+            var roles = await _roleRepository.GetAll().ToListAsync();
+                var users= await _userRepository.GetAll().Include(x=>x.UserRoles).Where(x=>x.Id==1).FirstOrDefaultAsync();
+            users.UserRoles = null;
+            users.Password = "user123";
+            _userRepository.InsertOrUpdateAndGetId(users);
+
+            var testNestedObj = users.MapTo<UserDto>();
+            return roles.MapTo<List<RoleDto>>();
+        }
+    }
+}
---
+++ AppService/Users/UserLoggedinAppService.cs
@@ -0,0 +1,72 @@
+﻿using AppDomain.Users;
+using AppDomain.Common;
+using AppService.Users.Dto;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppDomain;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using AppService.Common;
+
+namespace AppService.Users
+{
+    public interface IUserLoggedinAppService
+    {
+        Task<UserLoggedinOutputDto> UserAuthenticate(UserLoggedinInputDto input);
+    }
+    public class UserLoggedinAppService : IUserLoggedinAppService
+    {
+        private readonly IRepository<User, long> _userRepository;
+        #region Parameter
+        public UserLoggedinAppService(IRepository<User, long> userRepository)
+        {
+            this._userRepository = userRepository;
+        }
+        #endregion
+
+        public async Task<UserLoggedinOutputDto> UserAuthenticate(UserLoggedinInputDto input)
+        {
+            if (!string.IsNullOrEmpty(input.EmailAddress) && string.IsNullOrEmpty(input.Password)) return null;
+
+            var output = new UserLoggedinOutputDto();
+            var user = await _userRepository.GetAll().Include(x => x.UserRoles).ThenInclude(c => c.Role).FirstOrDefaultAsync(x => x.EmailAddress == input.EmailAddress && x.Password == input.EncryptedPassword);
+            if (user is null) return null;
+
+            user.Password = null;
+            output.Claims = new List<Claim> { new Claim(ClaimTypes.Email, user.EmailAddress), new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()) };
+
+            foreach (var uRole in user.UserRoles)
+            {
+                uRole.User = null;
+                uRole.Role.UserRoles = null;
+                output.Claims.Add(new Claim(ClaimTypes.Role, uRole.Role.Name));
+            }
+
+            output.User = user.MapTo<UserDto>();
+            return output;
+        }
+    }
+    public class UserLoggedinInputDto
+    {
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public bool IsPersistent { get; set; }
+        public string EncryptedPassword
+        {
+            get
+            {
+                return EncryptionHelper.Encrypt(Password);
+            }
+        }
+    }
+    public class UserLoggedinOutputDto
+    {
+        public UserDto User { get; set; }
+        public List<Claim> Claims { get; set; }
+}
+
+}
